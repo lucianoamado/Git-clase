17864f98ca8b422bbc3a85ef8727d16102e56ab5
thomas <thsrodriguez26@gmail.com>
2023-10-07 17:28:14 -0300

    actualizacionDepez

--- archivo1.js
+++ archivo1.js
@@ -1,2 +1,4 @@
 let uno = 1
 console.log(uno)
+
+
--- pez.js
+++ pez.js
@@ -1,4 +1,4 @@
 console.log("Mi pez se llama nemo");
 const fish = "spirit"
-const nombre = "pedro"
+const nombre = "pablo"
 console.log(`Mi pez se llama ${fish} y yo me llamo ${nombre}`)
